cc16774272358534c2607d77078896b535168d8f
Tyler2517 <tylerbrundage80@gmail.com>
2022-01-23 22:50:27 -0700

    Getting price of gpu

--- python/web_scrapping.py
+++ python/web_scrapping.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("index.html", "r") as f:
-    doc = BeautifulSoup(f, "html.parser")
+url = "https://www.newegg.com/gigabyte-geforce-rtx-3080-gv-n3080gaming-oc-10gd/p" \
+      "/N82E16814932459?Description=rtx%203080&cm_re=rtx_3080-_-14-932-459-_-Product"
+result = requests.get(url)
+doc = BeautifulSoup(result.text, "html.parser")
 
-print(doc)
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+strong = parent.find("strong")
+print(strong.string)
