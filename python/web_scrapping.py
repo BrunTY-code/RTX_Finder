256dc15b9a3302c0b4355cd1ac4f434a1f4137ac
Tyler2517 <tylerbrundage80@gmail.com>
2022-02-11 23:00:37 -0700

    Added text of item Added least to great Added formatting of the list

--- python/web_scrapping.py
+++ python/web_scrapping.py
@@ -10,6 +10,8 @@ doc = BeautifulSoup(result.text, "html.parser")
 page_text = doc.find(class_="list-tool-pagination-text").strong
 pages = int(str(page_text).split("/")[-2].split(">")[-1][:-1])
 
+all_items = {}
+
 for page in range(1, pages + 1):
     url = f"https://www.newegg.ca/p/pl?d={gpu}&N=4131&page={page}"
     page = requests.get(url).text
@@ -24,5 +26,16 @@ for page in range(1, pages + 1):
 
         link = parent["href"]
         next_parent = item.find_parent(class_="item-container")
-        price = next_parent.find(class_="price-current").strong.string
-        print(price)
+        try:
+            price = next_parent.find(class_="price-current").find("strong").string
+            all_items[item] = {"price": int(price.replace(",", "")), "link": link}
+        except:
+            print("Could not find price.")
+
+sorted_items = sorted(all_items.items(), key=lambda x: x[1]['price'])
+
+for item in sorted_items:
+    print(item[0])
+    print(f"${item[1]['price']}")
+    print(item[1]['link'])
+    print("-------------------------------")
